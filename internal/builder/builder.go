2bd5b4fc6d59f08680b1d922db06acef93a4a900
sushilchlgn <sushilchlgn@gmail.com>
2026-03-16 14:23:53 +0545

    module to handle multiple lang other then go with unzip

--- internal/builder/builder.go
+++ internal/builder/builder.go
@@ -1,6 +1,83 @@
 package builder
 
+import (
+	"archive/zip"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+// Builder interface defines standard methods for building and running projects
 type Builder interface {
-    Build(projectPath string) error
-    Run(projectPath string) error
+	Build(projectPath string) error
+	Run(projectPath string) error
+}
+
+// Unzip extracts a zip archive to the specified destination
+func Unzip(src string, dest string) error {
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+		fpath := filepath.Join(dest, f.Name)
+
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(fpath, os.ModePerm)
+			continue
+		}
+
+		if err := os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+			return err
+		}
+
+		outFile, err := os.Create(fpath)
+		if err != nil {
+			return err
+		}
+
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+
+		_, err = io.Copy(outFile, rc)
+		outFile.Close()
+		rc.Close()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// FindGoProjectRoot finds the folder containing main.go or go.mod
+func FindGoProjectRoot(base string) string {
+	if _, err := os.Stat(filepath.Join(base, "main.go")); err == nil {
+		return base
+	}
+	if _, err := os.Stat(filepath.Join(base, "go.mod")); err == nil {
+		return base
+	}
+
+	files, err := os.ReadDir(base)
+	if err != nil {
+		return base
+	}
+
+	for _, f := range files {
+		if f.IsDir() {
+			sub := filepath.Join(base, f.Name())
+			if _, err := os.Stat(filepath.Join(sub, "main.go")); err == nil {
+				return sub
+			}
+			if _, err := os.Stat(filepath.Join(sub, "go.mod")); err == nil {
+				return sub
+			}
+		}
+	}
+	return base
 }
